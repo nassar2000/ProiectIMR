dd4c57ce3d55e42d8bc8755fbd26d0ec7e4aa6fe
nassar2000 <61876347+nassar2000@users.noreply.github.com>
2020-11-29 18:14:12 +0200

    task3

---
+++ faceFilters/faceFilters/Assets/Scripts/ScrollView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ScrollView : MonoBehaviour
+{
+    private readonly List<GameObject> _items = new List<GameObject>();
+
+    public ARFaceManager aRFaceManager;
+
+    public void Filtru1(GameObject filter)
+    {
+        //aRFaceManager = gameObject.AddComponent<ARFaceManager>();
+        aRFaceManager.facePrefab = filter;
+    }
+}
---
+++ faceFilters/faceFilters/Assets/Scripts/UiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiController : MonoBehaviour
+{
+
+   public void StartApp(string scenaName)
+    {
+        SceneManager.LoadScene(scenaName);
+    } 
+}
