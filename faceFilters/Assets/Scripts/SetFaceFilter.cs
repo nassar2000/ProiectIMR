d6eb531c173ae9d545d9cbcffb7f869891b4f757
nassar2000 <61876347+nassar2000@users.noreply.github.com>
2020-12-12 21:41:33 +0200

    savePhoto

---
+++ faceFilters/Assets/Scripts/SetFaceFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+
+public class SetFaceFilter : MonoBehaviour
+{
+    public ARFaceManager aRFaceManager;
+    public ARSession aRSession;
+    public void Filtru1(GameObject filter)
+    {
+        aRSession.Reset();
+        aRFaceManager.facePrefab = filter;
+
+    }
+}
