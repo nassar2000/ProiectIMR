b2b738f169c5e72bf17eba443713bd49a5e2cde6
nassar2000 <61876347+nassar2000@users.noreply.github.com>
2021-01-03 01:08:22 +0200

    fix screen size

--- faceFilters/Assets/Scripts/TakeScreenshot.cs
+++ faceFilters/Assets/Scripts/TakeScreenshot.cs
@@ -2,24 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TakeScreenshot : MonoBehaviour {
+public class TakeScreenshot : MonoBehaviour
+{
 
-	[SerializeField]
-	GameObject blink;
 
-	public void TakeAShot()
-	{
-		StartCoroutine (CaptureIt());
-	}
 
-	IEnumerator CaptureIt()
-	{
-		string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
-		string fileName = "Screenshot" + timeStamp + ".png";
-		string pathToSave = fileName;
-		ScreenCapture.CaptureScreenshot(pathToSave);
-		yield return new WaitForEndOfFrame();
-		Instantiate (blink, new Vector2(0f, 0f), Quaternion.identity);
-	}
+    public void TakeAShot()
+    {
+        StartCoroutine(CaptureIt());
+    }
+
+    IEnumerator CaptureIt()
+    {
+
+        yield return new WaitForEndOfFrame();
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "/Screenshot" + timeStamp + ".png";
+        string path = Application.persistentDataPath + fileName;
+
+        Texture2D screenImage = new Texture2D(900, 1300, TextureFormat.RGB24, false);
+        //Get Image from screen
+        screenImage.ReadPixels(new Rect(100, 600, Screen.width, Screen.height), 0, 0);
+        screenImage.Apply();
+        //Convert to png
+        byte[] imageBytes = screenImage.EncodeToPNG();
+
+        //Save image to file
+        System.IO.File.WriteAllBytes(path, imageBytes);
+    }
 
 }
