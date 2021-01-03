5e73f3d9ef8ac85d952f3960a99a390e47ec0011
nassar2000 <61876347+nassar2000@users.noreply.github.com>
2021-01-03 16:16:44 +0200

    gallery prev

---
+++ faceFilters/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+
+   public void Loadscene(string scenaName)
+    {
+        SceneManager.LoadScene(scenaName);
+    } 
+}
